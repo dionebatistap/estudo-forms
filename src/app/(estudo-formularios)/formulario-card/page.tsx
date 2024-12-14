7b06c1da6b3880f3babbb38228426ab95477d372
dionepereira <120584913+dionepereira@users.noreply.github.com>
2024-12-14 11:49:31 -0300

    estrutura-basica-forms

---
+++ src/app/(estudo-formularios)/formulario-card/page.tsx
@@ -0,0 +1,7 @@
+export default function FormularioCard() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24 gap-4">
+      <h1 className="text-3xl">Formulário Card</h1>
+    </main>
+  );
+}
---
+++ src/app/(estudo-formularios)/formulario-sem-lib/page.tsx
@@ -0,0 +1,7 @@
+export default function FormularioBruto() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24 gap-4">
+      <h1 className="text-3xl">Formulário Sem Lib</h1>
+    </main>
+  );
+}
